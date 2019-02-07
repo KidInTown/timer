1c912a1e9124810ae4275b0a5627cff09ce3c1ec
KidInTown <42535795+KidInTown@users.noreply.github.com>
2019-02-07 19:12:37 +0200

    Delete ResetTimeButton.js

--- src/components/buttons/ResetTimeButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
- export default class ResetTimeButton extends React.Component {
-   render(){
-    return(
-      <div onClick = {this.props.resetTime} className = "btn btn-style reset"></div>
-    );
-  }
-}
