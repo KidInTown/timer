cfc97e351adcd7ce17d1d130bc8997f2a9af2d9d
KidInTown <42535795+KidInTown@users.noreply.github.com>
2019-02-07 18:53:52 +0200

    Create precache-manifest.068eb27e48bd8aa336ea6f3f7a050b63.js

---
+++ precache-manifest.068eb27e48bd8aa336ea6f3f7a050b63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6430496d5a87745e81dc",
+    "url": "/static/js/main.6430496d.chunk.js"
+  },
+  {
+    "revision": "8c633089612a270d805d",
+    "url": "/static/js/1.8c633089.chunk.js"
+  },
+  {
+    "revision": "6430496d5a87745e81dc",
+    "url": "/static/css/main.f1b51406.chunk.css"
+  },
+  {
+    "revision": "36f6150d5bbe706b0440d03b8147a6f4",
+    "url": "/index.html"
+  }
+];
