baeba9648f31d28d01c85689df50461d20de7470
KidInTown <42535795+KidInTown@users.noreply.github.com>
2019-02-07 19:12:40 +0200

    Delete SetTimeButton.js

--- src/components/buttons/SetTimeButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
- export default class SetTimeButton extends React.Component {
-   render(){
-    return(
-      <div tabIndex = "0" className = {this.props.classes.button.classSet} onClick = {this.props.setTime}></div>
-    );
-  }
-}
